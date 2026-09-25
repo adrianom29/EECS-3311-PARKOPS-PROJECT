df1282223e556065fb83450c6e050c038a5d8e2c
Adriano Mancuso <adriano.mancuso2906@gmail.com>
2026-09-24 20:01:02 -0400

    more formatting

--- src/DomainRules.java
+++ src/DomainRules.java
@@ -1,7 +1,12 @@
 public final class DomainRules {
-    private DomainRules() {}
+    private DomainRules() {
+
+    }
     public static void requireDuration(int hours) {
         // TODO D1: reject outside 1..24 with IllegalArgumentException.
+        if (hours < 1 || hours > 24){
+            throw new IllegalArgumentException("Inputted hours is out of bounds");
+        }
         throw new UnsupportedOperationException("D1 duration validation");
     }
     public static void requireIdentifier(String id) {
